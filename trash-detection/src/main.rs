71b1e4ec60d56050155800a78196f6b72383271b
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-02-09 14:26:02 -0800

    window sliding thing boilerplate

--- trash-detection/src/main.rs
+++ trash-detection/src/main.rs
@@ -1,11 +1,12 @@
 use threshold::ThresholdAlgorithm;
 
 mod convolution;
+mod wonkyslidy;
 mod threshold;
 mod algo3;
 mod hog;
 
 fn main()
 {
-    framework::run::<threshold::ThresholdAlgorithm>();
+    framework::run::<wonkyslidy::WonkySlidy>();
 }
